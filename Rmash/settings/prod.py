7f9ec4a528e121a4cb995f5d6351bfa6772607b6
Nitin <nitin8285824551@gmail.com>
2022-10-16 11:29:34 +0530

    fix .env file configuration and csrf trusted origins

--- Rmash/settings/prod.py
+++ Rmash/settings/prod.py
@@ -1,8 +1,5 @@
-"""
-Production Settings for Heroku
-"""
-
 import environ
+# import os
 
 # If using in your own project, update the project namespace below
 from Rmash.settings.base import * 
@@ -12,6 +9,9 @@ env = environ.Env(
     DEBUG=(bool, False)
 )
 
+# BASE_DIR = os.path.dirname(os.path.dirname(os.path.dirname(os.path.abspath(__file__))))
+# env.read_env(os.path.join(BASE_DIR, '.env'))
+
 # False if not in os.environ
 DEBUG = env('DEBUG')
 
@@ -19,10 +19,10 @@ DEBUG = env('DEBUG')
 SECRET_KEY = env('SECRET_KEY')
 
 ALLOWED_HOSTS = env.list('ALLOWED_HOSTS')
-CSRF_TRUSTED_ORIGINS = env.list('ALLOWED_HOSTS')
+CSRF_TRUSTED_ORIGINS = env.list('CSRF_TRUSTED_ORIGINS')
 
 # Parse database connection url strings like psql://user:pass@127.0.0.1:8458/db
 DATABASES = {
     # read os.environ['DATABASE_URL'] and raises ImproperlyConfigured exception if not found
     'default': env.db(),
-}
+} 
