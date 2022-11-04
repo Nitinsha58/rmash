3e86843552cd3b69d81f8f27589a7b97e422c79c
Nitin <nitin8285824551@gmail.com>
2022-11-04 20:17:12 +0530

    fix unhandeled error in api fetching

--- reportApp/page_speed_api.py
+++ reportApp/page_speed_api.py
@@ -7,32 +7,44 @@ CLIENT_SECRET = settings.CLIENT_SECRET
 API_KEY = settings.API_KEY
 
 def filterPageSpeedData(data=None):
-    categories_data = {}
-    audits_data = {}
-    categories = data["lighthouseResult"]["categories"]
-    audits = data["lighthouseResult"]["audits"]
+    error = True
+    try:
+        data["error"]
+    except KeyError:
+        error = False
 
-    for field in categories:
-        categories_data[field] = {
-                "title" : categories[field]["title"],
-                "score" : categories[field]["score"],
+    if error is not True:
+        categories_data = {}
+        audits_data = {}
+        categories = data["lighthouseResult"]["categories"]
+        audits = data["lighthouseResult"]["audits"]
+
+        for field in categories:
+            categories_data[field] = {
+                    "title" : categories[field]["title"],
+                    "score" : categories[field]["score"],
+                }
+
+        for field in ['uses-responsive-images', 'layout-shift-elements', 'lcp-lazy-loaded', 'timing-budget', 'http-status-code', 'largest-contentful-paint', 'cumulative-layout-shift', 'full-page-screenshot', 'robots-txt', 'image-alt']:
+            audits_data[field] = {
+                "id" : audits[field]["id"],
+                "title" : audits[field]["title"],
+                "score" : audits[field]["score"],
+                "scoreDisplayMode" : audits[field]["scoreDisplayMode"],
             }
 
-    for field in ['uses-responsive-images', 'layout-shift-elements', 'lcp-lazy-loaded', 'timing-budget', 'http-status-code', 'largest-contentful-paint', 'cumulative-layout-shift', 'full-page-screenshot', 'robots-txt', 'image-alt']:
-        audits_data[field] = {
-            "id" : audits[field]["id"],
-            "title" : audits[field]["title"],
-            "score" : audits[field]["score"],
-            "scoreDisplayMode" : audits[field]["scoreDisplayMode"],
+        filteredData = {
+            "id": data["id"],
+            "initial_url": data["loadingExperience"]["initial_url"],
+            "formFactor": data["lighthouseResult"]["configSettings"]["formFactor"],
+            "audits": audits_data,
+            "categories": categories_data,
+            "error": False,
         }
-    filteredData = {
-        "id": data["id"],
-        "initial_url": data["loadingExperience"]["initial_url"],
-        "formFactor": data["lighthouseResult"]["configSettings"]["formFactor"],
-        "audits": audits_data,
-        "categories": categories_data
-    }
-
+    else:
+        filteredData = {
+            "error": True
+        }
     return filteredData
 
 
