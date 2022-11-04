e13869e696b70e757368dc86d35686b96a62c65f
Nitin <nitin8285824551@gmail.com>
2022-11-05 01:55:21 +0530

    add create and fetch functionality

--- reportApp/views.py
+++ reportApp/views.py
@@ -1,9 +1,11 @@
 from django.shortcuts import render, redirect
 import re
-from  users.forms import UserAuthenticationForm
+from users.forms import UserAuthenticationForm
+from users.models import CustomUser
 from django.contrib.auth import login, logout
 from django.contrib.auth.decorators import login_required
 from .page_speed_api import get_data
+import json
 
 # Create your views here.
 
@@ -23,30 +25,43 @@ def index(request):
 @login_required(login_url='home')
 def logout_user(request):
     logout(request)
-    # messages.info(request, 'You logged out successfully')
     return redirect('home')
 
 @login_required(login_url='home')
 def dashboard(request):
-
     search_url = ''
+    err_msg = None
+    reports = request.user.report.all()
     if request.method == 'POST':
         search_url = request.POST['search']
         string = re.match('(?i)(url:|origin:)?http(s)?://.*', search_url)
+
         if not string:
             err_msg = "Urls must match the following example http(s)://example.com/"
             context = {'err_msg': err_msg, "search": search_url}
             return render(request, 'reportApp/dashboard.html', context)
+        elif reports.filter(report_id=search_url):
+            report = reports.filter(report_id=search_url)[0]
+            data = json.loads(report.report_data)
         else:
             data = get_data(search_url)
             if data["error"]:
-                err_msg = "Unable to get the data for " + search_url
+                err_msg = "Unable to get the data for" + search_url
                 data = None
             else:
-                err_msg = None
-            context = {'err_msg': err_msg, 'data': data, 'search':search_url}
-            return render(request, 'reportApp/dashboard.html', context)
+                str_report_data = json.dumps(data)
+                reportObj = request.user.report.create(user=request.user, report_id=data["id"], report_data=str_report_data)
+                reportObj.save()
 
+        context = {'err_msg': err_msg, 'data': data, 'search':search_url, 'reports': reports}
+        return render(request, 'reportApp/dashboard.html', context)
 
-    context = {'search': search_url}
+    context = {'search': search_url, 'reports': reports}
     return render(request, 'reportApp/dashboard.html', context)
+
+
+@login_required(login_url='home')
+def report(request, pk):
+
+    context = {}
+    return render(request, 'reportApp/report.html', context)
