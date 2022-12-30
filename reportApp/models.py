ee67886cd11fa7bde35941ccd2f5a4bc52186306
Nitin <nitin8285824551@gmail.com>
2022-12-30 19:20:32 +0530

    create api for report App

--- reportApp/models.py
+++ reportApp/models.py
@@ -4,6 +4,6 @@ from users.models import CustomUser
 # Create your models here.
 class Report(models.Model):
     user = models.ForeignKey(CustomUser, on_delete=models.CASCADE, related_name="report")
-    report_id = models.CharField(max_length=255, unique=True)
+    report_id = models.CharField(max_length=255)
     updated_at = models.DateTimeField(auto_now_add=True)
     report_data = models.TextField()
